81f3e608641a8705ad8e942789d00d27b95be5b4
Sergiyan92 <126787480+Sergiyan92@users.noreply.github.com>
2023-06-06 19:05:32 +0000

    Deploying to gh-pages from @ Sergiyan92/goit-react-hw-05-movies@05eb25bc1ac6bfedec9329b5bbc2c2382f271a64 🚀

--- static/js/415.c25527e1.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{415:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("h2",{children:"Trending today"})})}}}]);
-//# sourceMappingURL=415.c25527e1.chunk.js.map
--- static/js/884.f27a5352.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[884],{884:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:(0,a.jsx)("h2",{children:"MoviesDetails"})})}}}]);
-//# sourceMappingURL=884.f27a5352.chunk.js.map
